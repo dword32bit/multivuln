322253509fca65e0b51bf32a582014d31b604f51
Danang Tri Atmaja <114817148+dword32bit@users.noreply.github.com>
2025-04-24 00:50:49 +0700

    Update multivuln.sh

--- multivuln.sh
+++ multivuln.sh
@@ -27,35 +27,23 @@ cached_download() {
     fi
 }
 
-# Fungsi ekstrak ZIP
-extract_to_webroot() {
+# Fungsi ekstrak ZIP ke folder cache (tidak ke webroot langsung)
+extract_zip() {
     local file="$1"
-    local folder="$2"
-    local dest="$WEBROOT/$folder"
+    local tempdir="$CACHE_DIR/extracted_$(basename "$file" .zip)"
 
-    mkdir -p "$dest"
-    unzip -qo "$file" -d "$dest"
-    echo "[+] $folder installed."
-}
-
-# Git clone ke webroot
-git_clone_to_webroot() {
-    local repo="$1"
-    local folder="$2"
-
-    if [ ! -d "$WEBROOT/$folder" ]; then
-        git clone "$repo" "$WEBROOT/$folder"
-        echo "[+] $folder cloned."
-    else
-        echo "[+] $folder already cloned."
+    if [ ! -d "$tempdir" ]; then
+        mkdir -p "$tempdir"
+        unzip -q "$file" -d "$tempdir"
     fi
+    echo "$tempdir"
 }
 
 # Setup Apache
 setup_apache() {
     echo "[*] Setting up Apache web server..."
     apt-get update -qq && apt-get install -y apache2 php unzip git mariadb-server libapache2-mod-php php-mysqli > /dev/null
-    systemctl enable apache2 && systemctl start apache2
+    systemctl enable apache2 && systemctl restart apache2
     chown -R www-data:www-data "$WEBROOT"
     echo "[+] Apache configured."
 }
@@ -77,50 +65,49 @@ generate_index() {
     echo "[+] index.php created."
 }
 
-# Setup MySQL passwordless root (only if no password set)
+# Setup MySQL passwordless root
 secure_mysql() {
     echo "[*] Configuring MySQL root access..."
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''; FLUSH PRIVILEGES;" 2>/dev/null
+}
+
+# Fungsi generik untuk install aplikasi dari folder
+install_app() {
+    local zipname="$1"
+    local webdir="$2"
+    local dbname="$3"
+    local subdir="$4"
+
+    local tempdir=$(extract_zip "$CACHE_DIR/$zipname")
+    local sourcedir="$tempdir"
+
+    if [ -n "$subdir" ]; then
+        sourcedir="$tempdir/$subdir"
+    fi
+
+    mkdir -p "$WEBROOT/$webdir"
+    cp -r "$sourcedir"/* "$WEBROOT/$webdir/"
+    [ -n "$dbname" ] && mysql -u root -e "CREATE DATABASE IF NOT EXISTS $dbname;"
+    echo "[+] $webdir installed."
 }
 
 # Setup
 setup_apache
 secure_mysql
 
-# Aplikasi yang akan diinstall
-apps=(
-    "DVWA|https://github.com/digininja/DVWA/archive/refs/heads/master.zip|dvwa.zip|zip"
-    "bWAPP|https://downloads.sourceforge.net/project/bwapp/bwapp.zip|bwapp.zip|zip"
-    "Hackazon|https://github.com/rapid7/hackazon/archive/master.zip|hackazon.zip|zip"
-    "XVWA|https://github.com/s4n7h0/xvwa/archive/master.zip|xvwa.zip|zip"
-    "SecurityShepherd|https://github.com/OWASP/SecurityShepherd/archive/master.zip|shepherd.zip|zip"
-)
+# Download file yang dibutuhkan
+cached_download "https://github.com/digininja/DVWA/archive/refs/heads/master.zip" "dvwa.zip"
+cached_download "https://downloads.sourceforge.net/project/bwapp/bwapp.zip" "bwapp.zip"
+cached_download "https://github.com/rapid7/hackazon/archive/master.zip" "hackazon.zip"
+cached_download "https://github.com/s4n7h0/xvwa/archive/master.zip" "xvwa.zip"
+cached_download "https://github.com/OWASP/SecurityShepherd/archive/master.zip" "shepherd.zip"
 
-for entry in "${apps[@]}"; do
-    IFS='|' read -r name url filename type <<< "$entry"
-
-    echo "[*] Installing $name..."
-    cached_download "$url" "$filename"
-    case $type in
-        zip)
-            extract_to_webroot "$CACHE_DIR/$filename" "$name"
-            ;;
-        *)
-            echo "[!] Unknown type $type for $name"
-            ;;
-    esac
-    echo
-
-    # Database setup
-    if [[ "$name" == "XVWA" ]]; then
-        echo "[*] Setting up database for XVWA..."
-        mysql -u root -e "CREATE DATABASE IF NOT EXISTS xvwa;" 2>/dev/null
-    elif [[ "$name" == "SecurityShepherd" ]]; then
-        echo "[*] Setting up database for Security Shepherd..."
-        mysql -u root -e "CREATE DATABASE IF NOT EXISTS securityshepherd;" 2>/dev/null
-    fi
-
-done
+# Install aplikasi
+install_app "dvwa.zip" "DVWA" "dvwa" "DVWA-master"
+install_app "bwapp.zip" "bWAPP" "bWAPP" ""
+install_app "hackazon.zip" "Hackazon" "" "hackazon-master"
+install_app "xvwa.zip" "XVWA" "xvwa" "xvwa-master"
+install_app "shepherd.zip" "SecurityShepherd" "securityshepherd" "SecurityShepherd-master"
 
 generate_index
 
